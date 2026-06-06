6602ff3acbf10e848f7d94260b9b84f2244f483a
Shin Juheon <juheon8794@gmail.com>
2026-06-06 19:49:44 +0900

    LibraryManagerTest2.java 코드 작성 완료 및 주석 추가

--- LibraryManagement_v1.2/test/LibraryManagerTest2.java
+++ LibraryManagement_v1.2/test/LibraryManagerTest2.java
@@ -1,4 +1,10 @@
-// 원래 있던 LibraryManagerTest에 checkServerStatus에 대한 테스트 코드가 있어 중복되지 않게 새로운 테스트 파일 생성
+/**
+ * 원래 있던 LibraryManagerTest에 checkServerStatus에 대한 테스트 코드가 있어 중복되지 않게 새로운 테스트 파일 생성
+ * isValidIp에 대한 테스트 코드입니다.
+ * @see LibraryManager#isValidIp(java.lang.String)
+ * @see <a href="https://github.com/Shin-Juheon/LibraryManagement/issues/8">
+ */
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,10 +16,36 @@ class LibraryManagerTest2 {
                     new LibraryRepository());
 
     @Test
-    void 정상_IP_검사() {
+    void correctIpTest() {
 
         assertTrue(
                 manager.isValidIp(
                         "127.0.0.1"));
+    }
+    @Test
+    void commandInjectionTest() {
+
+        assertFalse(
+                manager.isValidIp(
+                        "127.0.0.1 && whoami")
+        );
+    }
+
+    @Test
+    void commandInjectionTest2() {
+
+        assertFalse(
+                manager.isValidIp(
+                        "127.0.0.1 & dir")
+        );
+    }
+
+    @Test
+    void commandInjectionTest3() {
+
+        assertFalse(
+                manager.isValidIp(
+                        "abc.def.ghi.jkl")
+        );
     }
 }
