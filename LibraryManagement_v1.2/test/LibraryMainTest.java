79ae64d0ec64181918cf39a8ad6fb3566147a43f
Shin Juheon <juheon8794@gmail.com>
2026-06-08 17:06:47 +0900

    userAccountLock 테스트 코드 작성 완료

--- LibraryManagement_v1.2/test/LibraryMainTest.java
+++ LibraryManagement_v1.2/test/LibraryMainTest.java
@@ -32,4 +32,31 @@ public class LibraryMainTest {
         }
         assertTrue(lockTimeMap.containsKey("admin"));
     }
+    @Test
+    void userAccountLock() {
+
+        Map<String, Integer> failCountMap = new HashMap<>();
+        Map<String, Long> lockTimeMap = new HashMap<>();
+
+        String id = "user01";
+
+        for(int i = 0; i < 5; i++) {
+
+            int failCount =
+                    failCountMap.getOrDefault(id, 0) + 1;
+
+            failCountMap.put(id, failCount);
+
+            if(failCount >= 5) {
+
+                long lockDuration = 3 * 60 * 1000;
+
+                lockTimeMap.put(
+                        id,
+                        System.currentTimeMillis() + lockDuration
+                );
+            }
+        }
+        assertTrue(lockTimeMap.containsKey("user01"));
+    }
 }
