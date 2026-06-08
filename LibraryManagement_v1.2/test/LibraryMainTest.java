e974cfafac9d5b81620e0a6d74e4e1f5872a9644
Shin Juheon <juheon8794@gmail.com>
2026-06-08 16:51:33 +0900

    adminAccountLock 메서드 작성

--- LibraryManagement_v1.2/test/LibraryMainTest.java
+++ LibraryManagement_v1.2/test/LibraryMainTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LibraryMainTest {
+
+    @Test
+    void adminAccountLock() {
+
+        Map<String, Integer> failCountMap = new HashMap<>();
+        Map<String, Long> lockTimeMap = new HashMap<>();
+
+        String id = "admin";
+
+        for(int i = 0; i < 5; i++) {
+
+            int failCount =
+                    failCountMap.getOrDefault(id, 0) + 1;
+
+            failCountMap.put(id, failCount);
+
+            if(failCount >= 5) {
+
+                long lockDuration = 10 * 60 * 1000;
+
+                lockTimeMap.put(
+                        id,
+                        System.currentTimeMillis() + lockDuration
+                );
+            }
+        }
+        assertTrue(lockTimeMap.containsKey("admin"));
+    }
 }
